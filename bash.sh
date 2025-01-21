a97835f93f15fc6d42040374d29a4590b1bd3e57
YANG Yizhi <bailiyunzhong@gmail.com>
2025-01-21 07:48:37 +0100

    max_bitwise may not function for doubles

--- bash.sh
+++ bash.sh
@@ -6,12 +6,14 @@ g++ -O BSM2.cxx -o BSM2
 ./BSM2 1000000 10
 ./BSM2 10000000 10
 ./BSM2 100000000 10
+rm -f BSM2
 
 g++ -Ofast -march=native BSM2p.cxx -o BSM2p -fopenmp 
 ./BSM2p 100000 10
 ./BSM2p 1000000 10
 ./BSM2p 10000000 10
 ./BSM2p 100000000 10
+rm -f BSM2p
 
 g++ -Ofast -march=native BSM.cxx -o BSM -fopenmp 
 # -I/opt/armpl/include -L/opt/armpl/lib -larmpl
@@ -20,3 +22,4 @@ g++ -Ofast -march=native BSM.cxx -o BSM -fopenmp
 ./BSM 1000000 10
 ./BSM 10000000 10
 ./BSM 100000000 10
+rm -f BSM
